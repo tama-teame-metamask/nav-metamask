de4e1ccde800d0244d6f668bdcd197ffe42df31f
mobinaHoseinpur <mobina.hoseinpur1377@gmail.com>
2023-10-03 18:57:37 +0330

    finish logo

--- js/mood.js
+++ js/mood.js
@@ -3,18 +3,23 @@ var element = document.body;
 const desktopBtn = document.querySelector('#themeBtnDesktop')
 desktopBtn.addEventListener('click' , function(){
     element.classList.toggle("Tdark");
+    if(element.classList=='Tdark'){
+        document.getElementById('logoImgDesktop').src="../assets/logodark.svg"
+    }else{
+        document.getElementById('logoImgDesktop').src="../assets/logolight.svg"
+    }
 })
 // themeBtnmobile
 const mobileBtn = document.querySelector('#themeBtnmobile')
 mobileBtn.addEventListener('click' , function(){
     if(element.classList=='Tdark'){
         element.classList.remove("Tdark")
-        document.getElementById('logoImg').src="";;
+        document.getElementById('logoImgMobile').src="../assets/logolight.svg"
         // imgLogo = 'assets/logolight.svg'
         
     }else{
         element.classList.add("Tdark")
-        document.getElementById('logoImg').src="";
+        document.getElementById('logoImgMobile').src="../assets/logodark.svg"
         // imgLogo = 'assets/logodark.svg'
     }
     }

dcbc51fd9abc1cc855bce4a53c209d965cbbf680
mobinaHoseinpur <mobina.hoseinpur1377@gmail.com>
2023-10-03 15:06:48 +0330

    done desktop&mobile  Complexity has been in toggle

--- js/mood.js
+++ js/mood.js
@@ -1,7 +1,16 @@
 var element = document.body;
 
-const test = document.querySelector('#themeBtn')
-console.log(test.value);
-test.addEventListener('click' , function(){
+const desktopBtn = document.querySelector('#themeBtnDesktop')
+desktopBtn.addEventListener('click' , function(){
     element.classList.toggle("Tdark");
 })
+// themeBtnmobile
+const mobileBtn = document.querySelector('#themeBtnmobile')
+mobileBtn.addEventListener('click' , function(){
+    if(element.classList=='Tdark'){
+        element.classList.remove("Tdark")
+    }else{
+        element.classList.add("Tdark")}
+    }
+    
+)
